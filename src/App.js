a06360440ba5df80ca816fe126b9eb744fcd3c4a
Hyeyun <yuuuuuuni@gmail.com>
2021-11-15 23:45:10 +0900

    init construction

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './asset/svg/moviesLogo.svg'
+import './App.scss';
+import './asset/style/common.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="logo" alt="logo" />
+        <div className="title size-20 bold">세상의 모든 영화</div>
       </header>
     </div>
   );
