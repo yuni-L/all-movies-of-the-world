b7959d6f6064d6adca3f904d023b6939428603f2
Hyeyun <yuuuuuuni@gmail.com>
2021-11-19 23:05:40 +0900

    init movie list

--- src/components/movies/movies.jsx
+++ src/components/movies/movies.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Modal from '../modal/modal'
 import './movies.scss'
 import 'asset/style/common.scss'
@@ -7,8 +7,17 @@ import 'asset/style/common.scss'
 const movies = () => {
   console.log('movies');
   const [ isShowModal, setIsShowModal ] = useState(false);
+  const [ movieList, setMovieLsit ] = useState([{title: '무비1'}, {title: '무비2'}, {title: '무비3'}, {title: '무비4'}]);
+  useEffect(() => {
+    // 무비 리스트 api 연결
+  }, [])
   return (
     <>
+      {
+        movieList.map((movie, index) => (
+          <div key={index} className='size-20'>{movie.title}</div>
+        ))
+      }
       <button className='size-20' onClick={() => setIsShowModal(true)}>모달 테스트 버튼</button>
       {isShowModal && <Modal setIsShowModal={setIsShowModal}/> }
     </>
