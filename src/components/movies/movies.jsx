7a31cdf498c6a6051587882eda4007117c0156bf
Hyeyun <yuuuuuuni@gmail.com>
2021-11-25 22:38:34 +0900

    fix: 네이버 api 환경변수 오류 해결

--- src/components/movies/movies.jsx
+++ src/components/movies/movies.jsx
@@ -14,26 +14,13 @@ import dayjs from "dayjs";
 const movies = ({ title, type }) => {
   const [isShowModal, setIsShowModal] = useState(false);
   const [movieList, setMovieList] = useState([]);
-  /**
-   * @todo 환경변수 오류 해결하기
-   */
+
   useEffect(async () => {
     loadMovieList();
-    await axios.get(
-      "/api/v1/search/movie.json",
-      {
-        headers: {
-          "X-Naver-Client-Id": process.env.REACT_APP_NAVER_CLIENT_ID,
-          "X-Naver-Client-Secret": process.env.REACT_APP_NAVER_CLIENT_SECRET_ID,
-        },
-        params: { query: '아이언맨' },
-      }
-    ).then((response) => {
-      console.log('rrrrr', response);
-    });
   }, []);
   /**
    * 영화 리스트 불러오기
+   * @todo 효율적인 로직 구현 필요
    */
   const loadMovieList = () => {
     switch (type) {
@@ -41,8 +28,9 @@ const movies = ({ title, type }) => {
         const yesterday = dayjs().subtract(1, "day").format("YYYYMMDD"); // api가 오늘 기준으로는 리스트 생성 불가
         const url = `http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${process.env.REACT_APP_MOVIES_API_KEY}&targetDt=${yesterday}`;
         axios.get(url).then((response) => {
-          console.log("response", response);
-          setMovieList(response.data.boxOfficeResult.dailyBoxOfficeList);
+          for(const movie of response.data.boxOfficeResult.dailyBoxOfficeList) {
+            // const test = loadMoviePoster(movie.movieNm);
+          }
         });
         break;
       }
@@ -50,6 +38,20 @@ const movies = ({ title, type }) => {
         break;
     }
   };
+  const loadMoviePoster = async (name) => {
+    await axios.get(
+      "/api/v1/search/movie.json",
+      {
+        headers: {
+          "X-Naver-Client-Id": process.env.REACT_APP_NAVER_CLIENT_ID,
+          "X-Naver-Client-Secret": process.env.REACT_APP_NAVER_CLIENT_SECRET_ID,
+        },
+        params: { query: name },
+      }
+    ).then((response) => {
+      return response.data.items[0].image;
+    });
+  }
   return (
     <>
       {movieList.length ? (
