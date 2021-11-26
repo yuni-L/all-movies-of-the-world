7920a60a6296f6f9eb10ef85522b1fc9c1dafcfb
Hyeyun <yuuuuuuni@gmail.com>
2021-11-26 23:48:24 +0900

    fix: linked movie image url

--- src/components/movies/movies.jsx
+++ src/components/movies/movies.jsx
@@ -5,6 +5,11 @@ import "asset/style/common.scss";
 import axios from "axios";
 import dayjs from "dayjs";
 
+const naverApiHeader = {
+  "X-Naver-Client-Id": process.env.REACT_APP_NAVER_CLIENT_ID,
+  "X-Naver-Client-Secret": process.env.REACT_APP_NAVER_CLIENT_SECRET_ID,
+}
+
 /**
  *
  * @param {String} title - 무비 라인 대표 제목
@@ -22,14 +27,17 @@ const movies = ({ title, type }) => {
    * 영화 리스트 불러오기
    * @todo 효율적인 로직 구현 필요
    */
-  const loadMovieList = () => {
+  const loadMovieList = async () => {
+    const movieNameList = [];
+    const movieInfoList = [];
     switch (type) {
       case "boxOffice": {
+        // 박스오피스 순위 불러오기
         const yesterday = dayjs().subtract(1, "day").format("YYYYMMDD"); // api가 오늘 기준으로는 리스트 생성 불가
         const url = `http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${process.env.REACT_APP_MOVIES_API_KEY}&targetDt=${yesterday}`;
-        axios.get(url).then((response) => {
+        await axios.get(url).then((response) => {
           for(const movie of response.data.boxOfficeResult.dailyBoxOfficeList) {
-            // const test = loadMoviePoster(movie.movieNm);
+            movieNameList.push(movie.movieNm);
           }
         });
         break;
@@ -37,21 +45,19 @@ const movies = ({ title, type }) => {
       default:
         break;
     }
+    for (const movieName of movieNameList) {
+      await axios.get(
+        "/api/v1/search/movie.json",
+        {
+          headers: naverApiHeader,
+          params: { query: movieName },
+        }
+      ).then((response) => {
+        movieInfoList.push(response.data.items[0]);
+      });
+    }
+    setMovieList(movieInfoList);
   };
-  const loadMoviePoster = async (name) => {
-    await axios.get(
-      "/api/v1/search/movie.json",
-      {
-        headers: {
-          "X-Naver-Client-Id": process.env.REACT_APP_NAVER_CLIENT_ID,
-          "X-Naver-Client-Secret": process.env.REACT_APP_NAVER_CLIENT_SECRET_ID,
-        },
-        params: { query: name },
-      }
-    ).then((response) => {
-      return response.data.items[0].image;
-    });
-  }
   return (
     <>
       {movieList.length ? (
@@ -60,7 +66,8 @@ const movies = ({ title, type }) => {
           <div className="movie-list">
             {movieList.map((movie, index) => (
               <div key={index} className="movie-list-item size-20">
-                {movie.movieNm}
+                <img src={movie.image} alt={movie.title}/>
+                {movie.title}
               </div>
             ))}
           </div>
