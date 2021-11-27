ceb624ca82c463779817c47a8236bd017853417a
Hyeyun <yuuuuuuni@gmail.com>
2021-11-27 21:56:07 +0900

    fix: modal layout

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,20 +1,24 @@
 /* eslint-disable react/style-prop-object */
 import React from "react";
+import { FaTimes } from "react-icons/fa";
 import "./modal.scss";
 import "asset/style/common.scss";
 
 const modal = ({ setIsShowModal, setClickedMovieInfo, clickedMovieInfo }) => {
   console.log("modal", clickedMovieInfo);
   return (
-    <div>
-      <button
-        onClick={() => {
-          setIsShowModal(false);
-          setClickedMovieInfo(null);
-        }}
-      >
-        닫기
-      </button>
+    <div className="back">
+      <div className="movie-modal">
+        <FaTimes
+        className="movie-modal-close f-right pointer"
+          onClick={() => {
+            setIsShowModal(false);
+            setClickedMovieInfo(null);
+          }}
+          size='30'
+        />
+        <div className="clear"></div>
+      </div>
     </div>
   );
 };
