15609220ec72c0b503ae0a15409d9e081a3dbb21
Hyeyun <yuuuuuuni@gmail.com>
2021-11-30 21:24:13 +0900

    fix: 영화 상세 정보 연결

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,29 +1,45 @@
-/* eslint-disable react/style-prop-object */
-import React from "react";
+import React, { useEffect } from "react";
 import { FaTimes } from "react-icons/fa";
 import "./modal.scss";
 import "asset/style/common.scss";
+import axios from "axios";
 
 const modal = ({ setIsShowModal, setClickedMovieInfo, clickedMovieInfo }) => {
-  console.log("modal", clickedMovieInfo);
+  useEffect(() => {
+    const url = `http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&ServiceKey=${process.env.REACT_APP_KMDB_API_KEY}&query=${clickedMovieInfo.title}&detail=Y&sort=prodYear,1`;
+    axios.get(url).then((response) => {
+      const { genre, nation, plots: { plot }, rating, stlls } = response.data.Data[0].Result[0];
+      clickedMovieInfo.actor = clickedMovieInfo.actor.split('|');
+      clickedMovieInfo.director = clickedMovieInfo.director.split('|');
+      setClickedMovieInfo({ ...clickedMovieInfo, genre, nation, plot: plot[0], rating, stlls: stlls.split('|') });
+    });
+  }, []);
+  useEffect(() => {
+    console.log("clickedMovieInfo", clickedMovieInfo);
+  }, [clickedMovieInfo]);
   return (
     <div className="back">
       <div className="movie-modal">
         <FaTimes
-        className="movie-modal-close f-right pointer"
+          className="movie-modal-close f-right pointer"
           onClick={() => {
             setIsShowModal(false);
             setClickedMovieInfo(null);
           }}
-          size='30'
+          size="30"
         />
         <div className="clear"></div>
         {/* movie-modal-contents start */}
         <div className="movie-modal-contents">
-          <span className="size-30 bold">{clickedMovieInfo.title}</span><br />
+          <span className="size-30 bold">{clickedMovieInfo.title}</span>
+          <br />
           <span className="size-20">{clickedMovieInfo.subtitle}</span>
           <div className="movie-modal-contents-info">
-            <img className="movie-modal-contents-info-img" src={clickedMovieInfo.image} alt={clickedMovieInfo.title}/>
+              <img
+                className="movie-modal-contents-info-img"
+                src={clickedMovieInfo.image}
+                alt={clickedMovieInfo.title}
+              />
             <div>ssss</div>
           </div>
         </div>
